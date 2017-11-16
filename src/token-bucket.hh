3fc85e90139c65769e4c624fdf2ad980ffa610fb
nanozhang <444051232@qq.com>
2017-11-16 11:29:08 +0800

    token-bucket

---
+++ src/token-bucket.hh
@@ -0,0 +1,54 @@
+#ifndef TOKENBUCKET_HH
+#define TOKENBUCKET_HH
+
+#include <vector>
+#include <deque>
+
+class Token_Bucket
+{
+private:
+    double _token_rate;
+    std::vector< double > _token;
+    std::vector< double > _last_tickno;
+    
+    double _max_token;
+
+public:
+    Token_Bucket(const double token_rate = 10,double max_token = 600):_token_rate(token_rate),_token(),_last_tickno(),_max_token(max_token){};
+
+    void autosize(const unsigned int id){
+        if(_token.size() <= id){
+            _token.resize( id + 1 );
+            _last_tickno.resize( id + 1 );
+        }
+    }
+    void set_rate(const double rate){
+        _token_rate = rate;
+    }
+    void set_max_token(const unsigned int max_token){
+        _max_token = max_token;
+    }
+    void get_token(const unsigned int src ,const double &tickno){
+        autosize(src);
+
+        double delta = tickno - _last_tickno[src];
+        _token[src] += delta*_token_rate;
+        _token[src] = _token[src]>_max_token?_max_token:_token[src];
+        _last_tickno[src] = tickno;
+    }
+    bool used_token(const unsigned int src,const double &tickno){
+        get_token(src,tickno);
+        if(_token[src]<1) return false;
+        _token[src] --;
+        return true;
+    }
+    void reset(double token_rate = -1)
+    {
+        _token.clear();
+        _last_tickno.clear();
+        if(token_rate > 0)
+            _token_rate = token_rate;
+    }
+};
+
+#endif
